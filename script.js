f9aa5e8de7472766b5a79d5c5f2505151c31a88f
Supro Ghosh <71887564+supro-180@users.noreply.github.com>
2020-09-29 11:03:03 +0600

    added html, css & js files

---
+++ script.js
@@ -0,0 +1,14 @@
+function showMenu(){
+    var x = document.getElementById("mobileMenu");
+    if(x.style.height == "0px"){
+        x.style.height = "250px";
+    }
+
+    else{
+        x.style.height = "0px"
+    }
+}
+
+// Simply just using the javascript. Its become dynamic with the menu-icon. //
+
+
